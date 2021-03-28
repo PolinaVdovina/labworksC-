af6a18fdc17616ea572d76e487e6cecfacdfe482
PolinaVdovina <polina.vdovina.98@inbox.ru>
2021-03-28 12:47:12 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace laba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ResearchTeam a = new ResearchTeam();
+            Console.WriteLine(a.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("Year: " + TimeFrame.Year);
+            Console.WriteLine("TwoYears: " + TimeFrame.TwoYears);
+            Console.WriteLine("Long: " + TimeFrame.Long);
+            Console.WriteLine();
+
+            a.Info = TimeFrame.Long;
+            a.Organization = "MyOrg";
+            a.RegNum = 220202;
+            a.Topic = "NewTopic";
+
+            Console.WriteLine(a.ToString());
+            Console.WriteLine();
+
+            a.AddPapers(new Paper("Title1", new Person("Mike", "Berns", new DateTime(1968, 12, 7)), new DateTime(2008, 7, 25)));
+            Console.WriteLine(a.ToString());
+
+            Console.WriteLine("LAST PAPER");
+            Console.WriteLine(a.LastPaper.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Выберите разделитель:");
+            string c = Console.ReadLine();
+            Console.WriteLine("Введите размер массивов:");
+            string[] mas = Console.ReadLine().Split(c);
+            int n = int.Parse(mas[0]);
+            int m = int.Parse(mas[1]);
+            ResearchTeam[] mas1 = new ResearchTeam[n * m];
+            for (int i = 0; i < n * m; i++)
+                mas1[i] = new ResearchTeam();
+            ResearchTeam[,] mas2 = new ResearchTeam[n, m];
+            for (int i = 0; i < n; i++)
+                for (int k = 0; k < m; k++)
+                    mas2[i,k] = new ResearchTeam();
+            ResearchTeam[][] mas3 = new ResearchTeam[n][];
+            for (int i = 0; i < n; i++)
+            {
+                mas3[i] = new ResearchTeam[m];
+                for (int k = 0; k < m; k++)
+                    mas3[i][k] = new ResearchTeam();
+            }
+
+            int time1 = 0;
+            int time2 = 0;
+            int time3 = 0;
+
+            int t1 = Environment.TickCount;
+            for (int i = 0; i < n * m; i++)
+                mas1[i].Topic = "ii";
+            int t2 = Environment.TickCount;
+            time1 = t2 - t1;
+
+            t1 = Environment.TickCount;
+            for (int i = 0; i < n; i++)
+                for (int k = 0; k < m; k++)
+                    mas2[i, k].Topic = "ii";
+
+            t2 = Environment.TickCount;
+            time2 = t2 - t1;
+
+            t1 = Environment.TickCount;
+            for (int i = 0; i < n; i++)
+                for (int k = 0; k < m; k++)
+                    mas3[i][k].Topic = "ii";                   
+            t2 = Environment.TickCount;
+            time3 = t2 - t1;
+
+            Console.WriteLine("Одномерный " + time1 + " Двумерный " + time2 + " Ступенчатый " + time3);
+        }
+    }
+}
---
+++ ResearchTeam.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace laba1
+{
+    class ResearchTeam
+    {
+        string topic;
+        string organization;
+        int regNum;
+        TimeFrame info;
+        Paper[] papersList;
+
+        public string Topic
+        {
+            get { return topic; }
+            set { topic = value; }
+        }
+
+        public string Organization
+        {
+            get { return organization; }
+            set { organization = value; }
+        }
+
+        public int RegNum
+        {
+            get { return regNum; }
+            set { regNum = value; }
+        }
+
+        public TimeFrame Info
+        {
+            get { return info; }
+            set { info = value; }
+        }
+
+        public Paper[] PapersList
+        {
+            get { return papersList; }
+            set { papersList = value; }
+        }
+
+        public ResearchTeam(string topic, string org, int regNum, TimeFrame info)
+        {
+            Topic = topic;
+            Organization = org;
+            RegNum = regNum;
+            Info = info;
+            papersList = new Paper[] { new Paper() };
+        }
+
+        public ResearchTeam()
+        {
+            Topic = "NoTopic";
+            Organization = "NoNameOrg";
+            RegNum = 0;
+            Info = 0;
+            papersList = new Paper[] { new Paper() };
+        }
+
+        public Paper LastPaper
+        {
+            get { return papersList[papersList.Length - 1]; }
+        }
+
+        public bool this[TimeFrame timeFr]
+        {
+            get
+            {
+                if (timeFr == info) return true;
+                else return false;
+            }
+        }
+
+        public void AddPapers(params Paper[] p)
+        {
+            Paper[] temp = new Paper[papersList.Length + p.Length];
+            temp = papersList.Concat(p).ToArray();
+            papersList = temp;
+        }
+
+        public override string ToString()
+        {
+            string s = "Topic: " +  topic + "; Organization: " + organization + "; RegNum: " + regNum + "; Info: " + info + "\n" + "Papers\n";
+            for (int i = 0; i < papersList.Length; i++)
+            {
+                s += papersList[i].ToString();
+                s += "\n";
+            }
+            return s;
+        }
+
+        public virtual string ToShortString()
+        {
+            string s = topic + " " + organization + " " + regNum + " " + info;
+            return s;
+        }
+    }
+}
